eae688174a73148a674c9b797fe44b490ee9a083
Nick Kibysh <nick.kibysh@nordicsemi.no>
2024-02-09 22:27:51 +0100

    created extension project

---
+++ DictionaryScrapper/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DictionaryScrapper
+//
+//  Created by Nick Kibysh on 09/02/2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
